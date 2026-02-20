8398d20cfedf1943a9c0bcc232fb1c73b8a84085
GhanshyamJangir <ghanshyamjangir334@gmail.com>
2026-02-20 19:11:14 +0530

    Update backend

--- src/db.js
+++ src/db.js
@@ -1,16 +1,9 @@
 // src/db.js
 const { Pool } = require("pg");
 
-// IMPORTANT: allow self-signed chain (Windows + pooler issue)
-if (process.env.NODE_TLS_REJECT_UNAUTHORIZED === "0") {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-}
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.PGSSL === "true"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on("error", (err) => {
